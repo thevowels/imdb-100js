f94a4251d936788f1e919690d93d0e2f5beb6fbc
thevowels <aungphyohtet1@gmail.com>
2024-10-01 18:19:51 +0630

    Learned a little bit about hydration error & how to solve

--- src/components/DarkModeSwitch.tsx
+++ src/components/DarkModeSwitch.tsx
@@ -1,7 +1,30 @@
+"use client"
+
+import {MdLightMode} from "react-icons/md";
+import {BsMoonFill} from "react-icons/bs";
+import {useEffect, useState} from "react";
+import { useTheme } from "next-themes";
 export default function DarkModeSwitch() {
+
+    const {systemTheme, theme, setTheme} = useTheme();
+    const [mounted, setMounted] = useState(false);
+
+    const currentTheme = theme === "system" ? systemTheme : theme;
+
+    useEffect(() => {
+        setMounted(true);
+    }, []);
+
     return(
-        <div>
-            DarkModeSwitch
-        </div>
+        <>
+            { mounted && (currentTheme === "dark"  ? (
+                <MdLightMode className={"text-xl cursor-pointer hover:text-amber-500"} onClick={() => setTheme("light")}/>
+                ):
+                (
+                    <BsMoonFill className={"text-xl cursor-pointer hover:text-amber-500"} onClick={() => setTheme("dark")}/>
+                )
+            )
+            }
+        </>
     )
 }
--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -5,7 +5,7 @@ import {ThemeProvider} from "next-themes";
 export default function Providers({children}: {children: React.ReactNode}) {
 
     return(
-        <ThemeProvider enableSystem={true} attribute={"class"}>
+        <ThemeProvider enableSystem={true} attribute="class">
             {children}
         </ThemeProvider>
     )
